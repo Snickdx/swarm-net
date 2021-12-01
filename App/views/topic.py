addd836fa5a88102b3498833ed1f77bd28368d64
JoSlam <joshualamb13@yahoo.com>
2021-11-30 21:29:15 -0400

    Post merge fixes

--- App/views/topic.py
+++ App/views/topic.py
@@ -3,66 +3,6 @@ from flask_jwt import jwt_required
 
 topic_views = Blueprint('topic_views', __name__, template_folder='../templates')
 
-from App.controllers import (
-     create_topic,
-     update_topic,
-     get_topic_by_id,
-     delete_topic_by_id,
-     get_topics,
-     get_topic_by_level
-
-)
-
- # create Topic
-@topic_views.route('/create-topic', methods=["POST"])
-@jwt_required()
-def create_user_topic():
-     topic_data = request.json.get('topic_data')
-     topic_level = request.json.get('topic_level')
-     newTopic = create_topic(topic_data, topic_level)
-     return jsonify(Topic.toDict())
-
- # update Topic
-@topic_views.route('/update-topic', methods=["POST"])
-@jwt_required()
-def update_user_topic():
-     topic_data = request.json.get('topic_data')
-     topic_level = request.json.get('topic_level')
-     topic_id = request.args.get("topic_id")
-     update_topic = create_topic(topic_id,topic_data,topic_level)
-     return jsonify(Topic.toDict())
-
- # get specific Topic by ID
-@topic_views.route('/topic_id', methods=["GET"])
-def get_topic_id():
-     topic_id = request.args.get("id")
-     topic = get_topic_by_id(topic_id)
-     return jsonify(Topic.toDict())
-
-
-#Deletes topic from database and returns if it was successful
-@topic_views.route('/delete-subTopic', methods=["DELETE"])
-def delete_subTopic():
-     topic_id = request.args.get("topic_id")
-     deleted = delete_topic_by_id(topic_id)
-     return jsonify({"deleted" : deleted})
-
-# get all topics
-@topic_views.route('/topics', methods=["GET"])
-@jwt_required()
-def display_topics():
-    topiclist = get_topics()
-    return jsonify(topiclist)
-
-
- # get  Topic by level id
- #not entirely sure about this method
-@topic_views.route('/topic_level', methods=["GET"])
-def get_topic_levelid():
-     topic_level = request.args.get("topic_level")
-     level = get_topic_by_level(topic_level)
-     return jsonify(Topic.toDict())
-=======
 from App.controllers.topic import (
     edit_topic,
     get_popular_topics,
@@ -122,23 +62,3 @@ def delete_topic(topic_id):
         return jsonify(result)
     else:
         return 404
-
-# TODO: Move to separate view
-# create SubTopic
-# @topic_views.route('/create-topic', methods=["POST"])
-# @jwt_required()
-# def create_Subtopic():
-#     #Maintopic_id
-#     Subtopic_name = request.json.get('Subtopic_name')
-#     Subtopic_description = request.json.get('Subtopic_description')
-#     #date_timestamp
-#     newTopic = create_subTopic(Maintopic_id, Subtopic_id, Subtopic_name,Subtopic_description,date_timestamp)
-#     return jsonify(SubTopic.toDict())
-
-
-#Deletes topic from database and returns if it was successful
-# @topic_views.route('/delete-subTopic', methods=["DELETE"])
-# def delete_subTopic():
-#     subTopic_id = request.args.get("sub_topic_id")
-#     deleted = delete_topic_by_id(subTopic_id)
-#     return jsonify({"deleted" : deleted})
