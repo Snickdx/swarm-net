5e51000e3450e8ed23098895a826ee2e0e69f684
JoSlam <joshualamb13@yahoo.com>
2021-11-30 23:22:10 -0400

    Feat: Added tag views and conroller Modified tag model

--- App/models/tag.py
+++ App/models/tag.py
@@ -7,12 +7,7 @@ db = SQLAlchemy()
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(300), nullable=False)
-    created = db.Column(db.Boolean, default=False, nullable=False)
-    
-
-    def __init__(self, text, created):
-        self.text = text
-        self.created = created
+    created = db.Column(db.DateTime, nullable=False)
         
 
     def __repr__(self):
