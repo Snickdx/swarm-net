8c438402f446466876186369e05a28c412a69dcf
JoSlam <joshualamb13@yahoo.com>
2021-12-09 23:31:28 -0400

    Fix: Updated chatroom scripts and user model

--- App/models/user.py
+++ App/models/user.py
@@ -1,8 +1,9 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name =  db.Column(db.String, nullable=False)
     last_name =  db.Column(db.String, nullable=False)
